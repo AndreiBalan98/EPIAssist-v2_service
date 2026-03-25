bfc83edacea30368853a64f90b3a779ac235baa3
Andrei Balan <andrei.balan98@gmail.com>
2026-03-25 18:36:41 +0200

    user prompt embedding

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from openai import OpenAI
 
 app = FastAPI()
+
+client = OpenAI(api_key="REMOVED")
 
 class MessageRequest(BaseModel):
     message: str
@@ -13,4 +16,15 @@ class MessageResponse(BaseModel):
 
 @app.post("/ai", response_model=MessageResponse)
 def ai(body: MessageRequest):
-    return MessageResponse(message=body.message)
+    
+    #1. enhance prompt
+    #2. convert to embedding
+    embedding = client.embeddings.create(
+        input=body.message,
+        model="text-embedding-3-small" #text-embedding-3-large
+    )
+
+    #3. retrieve top similar chunks
+    #4. generate answer
+
+    return MessageResponse(message=str(embedding.data[0].embedding))
